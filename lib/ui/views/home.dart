55422f92d3f2dcf9d2e8bf7588768e577fccbd46
Alex Ssanya <alexssanya@gmail.com>
2019-08-09 15:09:53 +0300

    Initial commit

---
+++ lib/ui/views/home.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  HomePage();
+
+  @override
+  State<StatefulWidget> createState() {
+    return _HomePageState();
+  }
+}
+    
+class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return null;
+  }
+
+}
